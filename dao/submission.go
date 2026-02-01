bd1a2a20e5e8538502dff21e70e20fb91c67b1fe
learning <77239050qq@gmail.com>
2026-02-01 13:15:57 +0800

    first version of SUBMISSIONSERVICE

--- dao/submission.go
+++ dao/submission.go
@@ -15,21 +15,22 @@ var Excellent = 1
 func (Sub *submission) CreateSub(s *model.Submission) error {
 	return DB.Create(&s).Error
 }
-func (Sub *submission) UpdateSub(s *model.Submission) error {
-	return DB.Save(&s).Error
-}
 func (Sub *submission) DeleteSub(s *model.Submission) error {
 	return DB.Delete(&s).Error
 }
-func (Sub *submission) MySubs(my int) (*[]model.Submission, error) {
+func (Sub *submission) MySubs(my string) (*[]model.Submission, error) {
 	var s []model.Submission
-	tx := DB.Where("CreatorID = ?", my).Find(&s)
+	me, err := UserDao.GetUserByName(my)
+	if err != nil {
+		return nil, err
+	}
+	tx := DB.Where("CreatorID = ?", me).Find(&s)
 	if tx.Error != nil {
 		return nil, tx.Error
 	}
 	return &s, nil
 }
-func (Sub *submission) DepartmentSubs(department int) (*[]model.Submission, error) {
+func (Sub *submission) DepartmentSubs(department model.Department) (*[]model.Submission, error) {
 	var s []model.Submission
 	tx := DB.Where("DepartmentID = ?", department).Find(&s)
 	if tx.Error != nil {
@@ -38,13 +39,14 @@ func (Sub *submission) DepartmentSubs(department int) (*[]model.Submission, erro
 	return &s, nil
 }
 
-func (Sub *submission) ChangeSub(s *model.Submission, score int, comment string, excellent int) error {
+func (Sub *submission) ChangeSub(s *model.Submission, reviewer string, score int, comment string, excellent int) error {
 	ex := excellent == Excellent
 	result := DB.Model(s).Where("Version = ?", s.Version).Updates(map[string]interface{}{
 		"Score":     score,
 		"Comment":   comment,
 		"excellent": ex,
 		"version":   gorm.Expr("Version + 1"),
+		"reviewer":  reviewer,
 	})
 	if result.Error != nil {
 		return result.Error
@@ -66,4 +68,17 @@ func (Sub *submission) DetectExcellent() (*[]uint, error) {
 		homeworks = append(homeworks, sub.HomeworkID)
 	}
 	return &homeworks, nil
+}
+func (Sub *submission) GetSub(title, name string) (model *model.Submission, err error) {
+	var sub model.Submission
+	u, err := UserDao.GetUserByName(name)
+	if err != nil {
+		//TODO
+	}
+	h, err := HomeworkDao.GetHomeworkByTitle(title)
+	if err != nil {
+		//TODO
+	}
+	DB.Where("StudentID = ?, HomeworkID = ?", u.ID, h.ID).First(&sub)
+	return &sub, nil
 }
--- service/submission.go
+++ service/submission.go
@@ -1,1 +1,73 @@
 package service
+
+import (
+	"homework_submit/dao"
+	"homework_submit/model"
+	"time"
+)
+
+type submission struct{}
+
+var Sub submission
+
+func (Sub *submission) CreateSub(creator, title string) error {
+	student, err := dao.UserDao.GetUserByName(creator)
+	if err != nil {
+		//TODO
+	}
+	homework, err := dao.HomeworkDao.GetHomeworkByTitle(title)
+	if err != nil {
+		//TODO
+	}
+	sub := model.Submission{
+		HomeworkID:  homework.ID,
+		StudentID:   student.ID,
+		SubmittedAt: time.Now(),
+		IsLate:      homework.Deadline.Before(time.Now()),
+	}
+	err = dao.SubDao.CreateSub(&sub)
+	if err != nil {
+		//TODO
+	}
+	return nil
+}
+func (Sub *submission) MySub(name string) (*[]model.Submission, error) {
+	subs, err := dao.SubDao.MySubs(name)
+	if err != nil {
+		//TODO
+	}
+	return subs, err
+}
+func (Sub *submission) DepartmentSub(department model.Department) (*[]model.Submission, error) {
+	subs, err := dao.SubDao.DepartmentSubs(department)
+	if err != nil {
+		//TODO
+	}
+	return subs, err
+}
+func (Sub *submission) ChangeSub(title, name, reviewer, comment string, score, excellent int) error {
+	sub, err := dao.SubDao.GetSub(name, reviewer)
+	if err != nil {
+		//TODO
+	}
+	err = dao.SubDao.ChangeSub(sub, reviewer, score, comment, excellent)
+	if err != nil {
+		//TODO
+	}
+	return nil
+}
+func (Sub *submission) ExcellentSub() (*[]model.Homework, error) {
+	excellents, err := dao.SubDao.DetectExcellent()
+	if err != nil {
+		//TODO
+	}
+	var homeworks []model.Homework
+	for _, excellent := range *excellents {
+		homework, err := dao.HomeworkDao.GetHomeworkByID(excellent)
+		if err != nil {
+			//TODO
+		}
+		homeworks = append(homeworks, *homework)
+	}
+	return &homeworks, nil
+}
