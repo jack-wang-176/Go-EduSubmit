e3bc94df3a64ee822e783338b43380d915ef71cc
learning <77239050qq@gmail.com>
2026-02-03 20:43:24 +0800

    implement of error

--- pkg/error.go
+++ pkg/error.go
@@ -1,8 +1,24 @@
 package pkg
 
+import "fmt"
+
 type CollectError struct {
 	Raw    error
 	Code   int
 	Status int
 	Msg    string
+}
+
+func (e *CollectError) Error() string {
+	return fmt.Sprintf("%d: %s", e.Code, e.Msg)
+}
+
+func (e *CollectError) Unwrap() error {
+	return e.Raw
+}
+func (e *CollectError) New(code int, msg string) *CollectError {
+	err := new(CollectError)
+	err.Code = code
+	err.Msg = msg
+	return err
 }
